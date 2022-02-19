6077b9a3606d39dfb505d4679eb50d8b349b84c7
onpilot <72357530+onpilot@users.noreply.github.com>
2022-02-20 00:01:08 +0700

    minor refactore

--- src/functions/getMinutesSeconds.js
+++ src/functions/getMinutesSeconds.js
@@ -1,8 +1,6 @@
-let minutes, seconds;
-
 export const getMinutesSeconds = (time) => {
-  minutes = parseInt(time / 60, 10);
-  seconds = parseInt(time % 60, 10);
+  let minutes = parseInt(time / 60, 10);
+  let seconds = parseInt(time % 60, 10);
 
   minutes = minutes < 10 ? '0' + minutes : minutes;
   seconds = seconds < 10 ? '0' + seconds : seconds;
