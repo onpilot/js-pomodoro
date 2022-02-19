6a934c74d614ae8ffa2f759947376610295ea27b
onpilot <72357530+onpilot@users.noreply.github.com>
2022-02-20 00:00:22 +0700

    revert to previous code since dividing it into smaller components makes some hooks don't work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
-import { ContainerTop } from './components/ContainerTop';
-import { ContainerBottom } from './components/ContainerBottom';
+import { Button } from './components/Button';
+import { Display } from './components/Display';
+import { Label } from './components/Label';
+import { Timerset } from './components/Timerset';
+import { Box } from './components/Box';
+import { FunctionSet } from './functions/FunctionSet';
+import { getMinutesSeconds } from './functions/getMinutesSeconds';
 import './App.css';
 
 function App() {
+  const { current, breakLen, sessionLen, timeLeft, decrease, increase, start_pause, reset } =
+    FunctionSet();
+
   return (
     <div className="app">
       <div className="app-title">25+5 aka Pomodoro Clock</div>
       <div className="app-main">
-        <ContainerTop className="main-top"></ContainerTop>
-        <ContainerBottom className="main-bottom"></ContainerBottom>
+        <div className="main-top">
+          <Timerset className="timerset">
+            <Label id="break-label" htmlFor="break-length">
+              break length
+            </Label>
+            <Box className="box">
+              <Button id="break-decrement" onClick={() => decrease('break')}>
+                -
+              </Button>
+              <Display id="break-length">{breakLen}</Display>
+              <Button id="break-increment" onClick={() => increase('break')}>
+                +
+              </Button>
+            </Box>
+          </Timerset>
+          <Timerset className="timerset">
+            <Label id="session-label" htmlFor="session-length">
+              session length
+            </Label>
+            <Box className="box">
+              <Button id="session-decrement" onClick={() => decrease('session')}>
+                -
+              </Button>
+              <Display id="session-length">{sessionLen}</Display>
+              <Button id="session-increment" onClick={() => increase('session')}>
+                +
+              </Button>
+            </Box>
+          </Timerset>
+        </div>
+        <div className="main-bottom">
+          <Label htmlFor="time-left" id="timer-label">
+            {current}
+          </Label>
+          <Display id="time-left">{getMinutesSeconds(timeLeft)}</Display>
+          <Box>
+            <Button id="start_stop" onClick={() => start_pause()}>
+              start/pause
+            </Button>
+            <Button id="reset" onClick={() => reset()}>
+              reset
+            </Button>
+          </Box>
+        </div>
       </div>
     </div>
   );
